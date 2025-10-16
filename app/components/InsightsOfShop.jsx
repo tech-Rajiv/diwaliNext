083a9dc4f1ef67f5fc5da58f1c04a8c6ec270d15
rajiv <contact.rajivmishra@gmail.com>
2025-10-16 16:01:14 +0530

    added download image feature

--- app/components/InsightsOfShop.jsx
+++ app/components/InsightsOfShop.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import MemberInfo from "./uiByMe/MemberInfo";
 import PieCharts from "./charts/PieCharts";
 import CustomPieChart from "./uiByMe/CustomPieChart";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 function InsightsOfShop({ data }) {
   console.log(data, "data shop");
@@ -13,11 +15,23 @@ function InsightsOfShop({ data }) {
         <div className="memberInfo  ">
           <MemberInfo data={data} />
         </div>
-        <div className="pie mt-5">
-          {/* <PieCharts data={data} />
-           */}
-          <CustomPieChart data={data} />
+        <div className="flex flex-col justify-center mt-5">
+          <Image
+            src="/bill.jpg"
+            height={100}
+            width={200}
+            className="w-full rounded-xl"
+            alt="shopBill"
+          />
+          <p className="mt-2 text-center">
+            <a href="/bill.jpg" download>
+              <Button>Download Image</Button>
+            </a>
+          </p>
         </div>
+        {/* <div className="pie mt-5">
+          <CustomPieChart data={data} />
+        </div> */}
       </div>
     </div>
   );
